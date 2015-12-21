04c61efff4894d3d049790ef52c55cfb7d06236d
zhangxin.zhang <zhangxin.zhang@qunar.com>
2015-12-21 15:43:55 +0800

    merge

---
+++ concurrent/src/main/java/net/jcip/annotations/GuardedBy.java
@@ -0,0 +1,10 @@
+package net.jcip.annotations;
+
+/**
+ * ↓↓↓↓↓↓↓↓↓↓描述↓↓↓↓↓↓↓↓↓↓
+ *
+ * @author zhangxin.zhang created on 15-12-20.
+ */
+public @interface GuardedBy {
+    String value();
+}
---
+++ concurrent/src/main/java/net/jcip/annotations/ThreadSafe.java
@@ -0,0 +1,9 @@
+package net.jcip.annotations;
+
+/**
+ * ↓↓↓↓↓↓↓↓↓↓描述↓↓↓↓↓↓↓↓↓↓
+ *
+ * @author zhangxin.zhang created on 15-12-20.
+ */
+public @interface ThreadSafe {
+}
