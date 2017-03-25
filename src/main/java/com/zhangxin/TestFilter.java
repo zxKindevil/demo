916af0de40e5b4cf9f0c2c224a3acb13874ed9c7
zhangxin <zhangxin@benmuhealth-inc.com>
2017-03-25 17:10:53 +0800

    up

---
+++ src/main/java/com/zhangxin/TestFilter.java
@@ -0,0 +1,21 @@
+package com.zhangxin;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author zhangxin
+ *         Created on 17/3/25.
+ */
+public class TestFilter extends OncePerRequestFilter {
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("preFilter");
+        filterChain.doFilter(httpServletRequest,httpServletResponse);
+        System.out.println("afterFilter");
+    }
+}
