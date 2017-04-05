a63be43fc0ec5937a9cc0b696fb775b2a7adefeb
zhangxin <zhangxin@benmuhealth-inc.com>
2017-04-05 17:34:13 +0800

    up

--- src/test/java/com/zhangxin/test/EsTest.java
+++ src/test/java/com/zhangxin/test/EsTest.java
@@ -1,62 +1,60 @@
 package com.zhangxin.test;
 
-import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.reindex.DeleteByQueryAction;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.io.Serializable;
-import java.net.InetAddress;
 import java.util.Date;
 import java.util.Map;
-
-import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 /**
  * @author zhangxin
  *         Created on 17/3/16.
  */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = "classpath:spring/applicationContext.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/applicationContext.xml")
 public class EsTest {
+    @Resource
     private TransportClient client;
 
     @Before
     public void init() throws Exception {
-        Settings settings = Settings.builder()
-                .put("cluster.name", "usearch").build();
-        client = new PreBuiltTransportClient(settings)
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.32.64.19"), 9302))
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.32.64.20"), 9302));
+//        Settings settings = Settings.builder()
+//                .put("cluster.name", "mts.elastic.index.center.beta1").build();
+//        client = new PreBuiltTransportClient(settings)
+//                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("10.32.64.13"), 8300));
     }
 
     @Test
     public void test() throws IOException {
-        IndexResponse response = client.prepareIndex("test", "orde11", "2")
-                .setSource(jsonBuilder()
-                        .startObject()
-                        .field("id", 2)
-                        .field("aaa", "fuck测试你妹的aaaa")
-                        .field("bbb", "bbb")
-                        .field("ccc", "sdfdsfssdfsdf水电费2234水电费水电费是放水电费水电费水电费水电费水电费水电费水电费水电费水电费水电费水电费我只是一个测试束带结发克里斯多夫级科室老地方级科室对方级科室对方就上课了打飞机是考虑到房间卡死了打飞机卡死了代缴费莱克斯顿及父类卡数据的福克斯来得及发可怜世纪东方考虑数据的反馈 ")
-                        .field("updateTime", new Date().getTime())
-                        .endObject()
-                )
-                .get();
+//        IndexResponse response = client.prepareIndex("test", "orde11", "2")
+//                .setSource(jsonBuilder()
+//                        .startObject()
+//                        .field("id", 2)
+//                        .field("aaa", "fuck测试你妹的aaaa")
+//                        .field("bbb", "bbb")
+//                        .field("ccc", "sdfdsfssdfsdf水电费2234水电费水电费是放水电费水电费水电费水电费水电费水电费水电费水电费水电费水电费水电费我只是一个测试束带结发克里斯多夫级科室老地方级科室对方级科室对方就上课了打飞机是考虑到房间卡死了打飞机卡死了代缴费莱克斯顿及父类卡数据的福克斯来得及发可怜世纪东方考虑数据的反馈 ")
+//                        .field("updateTime", new Date().getTime())
+//                        .endObject()
+//                )
+//                .get();
+//
+//        client.close();
 
-        client.close();
-
+        System.out.println(client.connectedNodes());
         System.out.println("here");
     }
 
