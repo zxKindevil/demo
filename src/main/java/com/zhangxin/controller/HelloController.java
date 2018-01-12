317568e5816b0a28b91f37da2c1be64cdc3e58a1
zhangxin <zhangxin@benmu-health.com>
2018-01-13 03:09:31 +0800

    ss

--- src/main/java/com/zhangxin/controller/HelloController.java
+++ src/main/java/com/zhangxin/controller/HelloController.java
@@ -9,7 +9,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +24,9 @@ import java.util.concurrent.TimeUnit;
  */
 @RestController
 public class HelloController implements InitializingBean {
+    static SimpleDateFormat yymmdd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public static String BUY = "https://otcbtc.com/sell_offers?currency=eth&fiat_currency=cny&payment_type=all";
-    public static String SALE = "https://www.otcbtc.com/buy_offers?currency=eth&fiat_currency=cny&payment_type=all";
+    public static String SALE = "https://otcbtc.com/buy_offers?currency=eth&fiat_currency=cny&payment_type=all";
 
     @RequestMapping("/")
     public String index() {
@@ -49,35 +49,28 @@ public class HelloController implements InitializingBean {
     public void deal() throws IOException, InterruptedException {
         Connection connect = Jsoup.connect(BUY);
 
+        Connection saleSession = Jsoup.connect(SALE);
+
         File file = new File("price.txt");
 
         while (!Thread.interrupted()) {
             try {
-                BigDecimal finalmin = BigDecimal.ZERO;
+                BigDecimal buymin;
                 BigDecimal finalshijia;
-                BigDecimal finalyijia;
+                BigDecimal salemax;
+                BigDecimal yijia;
+                BigDecimal chajia;
 
-                Document doc = connect.get();
-                Elements elements = doc.getElementsByClass("recommend-card__price");
-                BigDecimal min = new BigDecimal(Integer.MAX_VALUE);
-                for (Element element : elements) {
-                    String text = element.text();
-                    text = text.replaceAll(",", "");
-                    if (new BigDecimal(text).compareTo(min) < 0) {
-                        min = new BigDecimal(text);
-                        finalmin = min;
-                    }
-                }
+                Pair<BigDecimal, BigDecimal> buyPair = this.parseBuyHtml(connect);
+                salemax = this.parseSaleHtml(saleSession);
+                buymin = buyPair.getLeft();
+                finalshijia = buyPair.getRight();
 
-                Elements box = doc.getElementsByClass("box");
-                Elements select = box.select("span[class=price]");
-                String shijia = select.get(0).text();
-                shijia = shijia.replaceAll("CNY", "").replaceAll(" ", "").replaceAll(",", "");
-                finalshijia = new BigDecimal(shijia);
-                finalyijia = finalmin.subtract(finalshijia);
+                yijia = buymin.subtract(finalshijia);
+                chajia = buymin.subtract(salemax);
 
                 String format = yymmdd.format(new Date());
-                String join = Joiner.on(",").join(format, finalmin, finalshijia, finalyijia) + "\n";
+                String join = Joiner.on(",").join(format, buymin, finalshijia, yijia, salemax, chajia) + "\n";
                 System.out.println(join);
                 Files.append(join, file, Charsets.UTF_8);
                 TimeUnit.SECONDS.sleep(20);
@@ -85,11 +78,74 @@ public class HelloController implements InitializingBean {
                 //ignore
             }
         }
+    }
 
+    private BigDecimal parseSaleHtml(Connection saleSession) throws IOException {
+        Document doc = saleSession.get();
+        Elements elements = doc.getElementsByClass("recommend-card__price");
+        BigDecimal max = BigDecimal.ZERO;
+        for (Element element : elements) {
+            String text = element.text();
+            text = text.replaceAll(",", "");
+            if (new BigDecimal(text).compareTo(max) > 0) {
+                max = new BigDecimal(text);
+            }
+        }
+
+        return max;
+    }
+
+    public Pair parseBuyHtml(Connection connect) throws IOException {
+        Document doc = connect.get();
+        Elements elements = doc.getElementsByClass("recommend-card__price");
+        BigDecimal min = new BigDecimal(Integer.MAX_VALUE);
+        for (Element element : elements) {
+            String text = element.text();
+            text = text.replaceAll(",", "");
+            if (new BigDecimal(text).compareTo(min) < 0) {
+                min = new BigDecimal(text);
+            }
+        }
+
+        Elements box = doc.getElementsByClass("box");
+        Elements select = box.select("span[class=price]");
+        String shijia = select.get(0).text();
+        shijia = shijia.replaceAll("CNY", "").replaceAll(" ", "").replaceAll(",", "");
+
+
+        return Pair.of(min, new BigDecimal(shijia));
 
     }
 
-    static SimpleDateFormat yymmdd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
+}
 
+class Pair<L,R> {
+    L left;
+    R right;
+
+    public Pair(L left, R right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public static <L,R> Pair of(L left, R right){
+        return new Pair<>(left, right);
+    }
+
+    public L getLeft() {
+        return left;
+    }
+
+    public void setLeft(L left) {
+        this.left = left;
+    }
+
+    public R getRight() {
+        return right;
+    }
+
+    public void setRight(R right) {
+        this.right = right;
+    }
 }
