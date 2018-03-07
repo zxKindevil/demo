e4fa4910d9ae303d322a7a420b36f30c8d69c715
zhangxin <zhangxin@benmuhealth-inc.com>
2018-03-07 14:20:57 +0800

    up

--- src/main/scala/com/MainScala.scala
+++ src/main/scala/com/MainScala.scala
@@ -1,30 +1,44 @@
 package com
 
-import scala.collection.generic.CanBuildFrom
-import scala.collection.{BitSet, mutable}
+import java.io.File
+import java.util
+
+import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
+import com.google.common.base.Charsets
+import com.google.common.io.Files
+
+import scala.collection.JavaConversions._
+import scala.util.matching.Regex.Match
 
 /**
   * @author zhangxin
   *         Created on 18/3/1.
   */
 object MainScala {
+  //ohFAc0ZFL4ocY-D0ru625GOC838c
+  val pattern = "[a-zA-Z0-9-]+".r
+
   def main(args: Array[String]): Unit = {
-    val builder: mutable.SetBuilder[Int, Set[Int]] = new mutable.SetBuilder(Set())
-    builder.+=(1)
-    builder.+=(2)
-    builder.+=(3)
+    val jsonNode: JsonNode = new ObjectMapper().readTree("{\"productId\"    :\"110\",\"orderFrom\":\"CHECKUP_MINA\",\"checkupDate\":\"2018-03-09\",\"userInfo\":{\"patientName\":\"苟娇\",\"idCardNo\":\"513701199709222224\",\"phone\":\"15011013389\"},\"_\":1520303280553}")
+    println(jsonNode.get("orderFrom").textValue())
 
-    val result: Set[Int] = builder.result()
+    test()
+  }
 
-    Set(1, 2, 3) map (_ * 2)
+  def test(): Unit = {
+    val file: File = new File("/Users/zhangxin/temp.txt")
+    val lines: util.List[String] = Files.readLines(file, Charsets.UTF_8)
 
-    val hashSetBuilder = new CanBuildFrom[Set[Int], Int, mutable.HashSet[Int]] {
-      override def apply(from: Set[Int]): mutable.Builder[Int, mutable.HashSet[Int]] = apply()
+    lines.toList forEach (x => {
+      if (x.startsWith("[2018-03-06")) {
+        val openidRegex = "\\[([a-zA-Z0-9-_]+)\\]".r
+        val matches: Iterator[Match] = openidRegex findAllMatchIn x
+        val openid: String = matches.toList.apply(1).group(1)
+        //        println(openid)
 
-      override def apply(): mutable.Builder[Int, mutable.HashSet[Int]] = mutable.HashSet.newBuilder[Int]
-    }
-
-    println(Set(1, 2, 3).map(_ * 2)(hashSetBuilder).getClass)
+        val name: String = ("patientName\":\"(\\W+)\",".r() findAllMatchIn x).toList.apply(0).group(1)
+      }
+    })
   }
 }
 
