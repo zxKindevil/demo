421bfb7dde0756430b6201ea3f1a7a7300a18cce
zhangxin <zhangxin@benmuhealth-inc.com>
2018-03-08 16:19:39 +0800

    up

--- src/main/scala/com/MainScala.scala
+++ src/main/scala/com/MainScala.scala
@@ -18,34 +18,36 @@ object MainScala {
   val pattern = "[a-zA-Z0-9-]+".r
 
   def main(args: Array[String]): Unit = {
-    //    println((List(1, 2, 3) map (x => (x + 1, x + 2))).toMap)
-
     test()
   }
 
   def test(): Unit = {
     val file: File = new File("/Users/zhangxin/temp.txt")
-    val lines: util.List[String] = Files.readLines(file, Charsets.UTF_8)
+    val list: util.List[String] = Files.readLines(file, Charsets.UTF_8)
 
-    println(lines.toList.map(x =>
-      if (x.startsWith("[2018-03-06")) {
-        val openidRegex = "\\[([a-zA-Z0-9-_]+)\\]".r
-        val matches: Iterator[Match] = openidRegex findAllMatchIn x
-        val openid: String = matches.toList.apply(1).group(1)
-        //        println(openid)
+    val lines: List[String] = list.toList.filter(x => x.startsWith("[2018-03-06"))
 
-        val name: String = ("patientName\":\"(\\W+)\",".r() findAllMatchIn x).toList.apply(0).group(1)
 
-        return (openid, name)
-      }
-      else ("a", "b")
-    ).toMap)
-  }
+    val mutilmap: util.HashMap[String, List[String]] = new util.HashMap[String, List[String]]
 
-  def testb(): Unit = {
-    //    val list = List("this", "maps", "string", "to", "length") map { s => (s, s.length) }
-    //    val list = List("this", "is", "a", "bunch", "of", "strings")
-    //    val string2Length = Map(list map { s => (s, s.length) }: _*)
+    val toList: List[(String, String)] = lines.map(x => {
+      val openidRegex = "\\[([a-zA-Z0-9-_]+)\\]".r
+      val matches: Iterator[Match] = openidRegex findAllMatchIn x
+      val openid: String = matches.toList.apply(1).group(1)
+
+      val name: String = ("patientName\":\"(\\W+)\",".r() findAllMatchIn x).toList.apply(0).group(1)
+
+      var list: List[String] = mutilmap.getOrDefault(openid, List())
+      list = list.+:(name)
+
+      mutilmap.put(openid, list)
+
+      (openid, name)
+    })
+
+    mutilmap.forEach((x, y) => {
+      println((x, y))
+    })
+
   }
 }
-
