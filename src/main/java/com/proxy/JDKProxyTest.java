2f455b830ed2ad3045b86b1f9f0e16af45bfdd4f
zhangxin <zhangxin@benmuhealth-inc.com>
2018-07-25 20:27:32 +0800

    up

---
+++ src/main/java/com/proxy/JDKProxyTest.java
@@ -0,0 +1,67 @@
+package com.proxy;
+
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * @author zhangxin
+ *         Created on 18/7/25.
+ */
+public class JDKProxyTest {
+    public static void main(String[] args) {
+        String path = "/Users/zhangxin/z/ideaProject/dev/temp/demo/ProxyGTest.class";
+        byte[] classFile = ProxyGenerator.generateProxyClass("$Proxy0",
+                HelloImpl.class.getInterfaces());
+        FileOutputStream out = null;
+
+        try {
+            out = new FileOutputStream(path);
+            out.write(classFile);
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+//        HelloService hello = new HelloImpl();
+//        HelloService proxy = (HelloService) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class[]{HelloService.class}, new HelloProxy(hello));
+//        proxy.say();
+    }
+}
+
+interface HelloService {
+    void say();
+}
+
+class HelloImpl implements HelloService {
+
+    @Override
+    public void say() {
+        System.out.println("执行业务方法");
+    }
+}
+
+class HelloProxy implements InvocationHandler {
+    private HelloService helloService;
+
+    public HelloProxy(HelloService business) {
+        this.helloService = business;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("before invoke");
+        Object result = method.invoke(helloService, args);
+        System.out.println("after invoke");
+        return result;
+    }
+}
