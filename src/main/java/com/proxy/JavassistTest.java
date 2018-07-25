15d9031c83cf540a16d22f8b39e637d78bc5b8cb
zhangxin <zhangxin@benmuhealth-inc.com>
2018-07-25 21:20:51 +0800

    up

---
+++ src/main/java/com/proxy/JavassistTest.java
@@ -0,0 +1,25 @@
+package com.proxy;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.CtNewMethod;
+
+/**
+ * @author zhangxin
+ *         Created on 18/7/25.
+ */
+public class JavassistTest {
+    public static void main(String[] args) throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        //创建Programmer类
+        CtClass cc= pool.makeClass("com.samples.Programmer");
+        //定义code方法
+        CtMethod method = CtNewMethod.make("public void code(){}", cc);
+        //插入方法代码
+        method.insertBefore("System.out.println(\"I'm a Programmer,Just Coding.....\");");
+        cc.addMethod(method);
+        //保存生成的字节码
+        cc.writeFile("/Users/zhangxin/z/ideaProject/dev/temp/demo/temp");
+    }
+}
