76c9a95b86e6a70989f282ef23a1c79bb6cf7020
zhangxin <zhangxin@benmuhealth-inc.com>
2018-08-22 15:05:44 +0800

    up

--- src/test/java/com/dubbo/test/ConsumerMain.java
+++ src/test/java/com/dubbo/test/ConsumerMain.java
@@ -8,33 +8,45 @@ import com.dubbo.test.service.HelloService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * @author zhangxin.zhang created on 16-6-23.
  */
 public class ConsumerMain {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:spring-consumer-root.xml"});
         context.start();
 
-        HelloService helloService = (HelloService) context.getBean("helloService");
-        helloService.say();
+        final HelloService helloService = (HelloService) context.getBean("helloService");
+//        helloService.say();
+//
+//        FutureAdapter future = (FutureAdapter) RpcContext.getContext().getFuture();
+//        future.getFuture().setCallback(new ResponseCallback() {
+//            public void done(Object o) {
+//                RpcResult rpcResult = ((RpcResult) o);
+//                if (rpcResult.hasException()) {
+//                    //log
+//                } else {
+//                    System.out.println(rpcResult.getValue());
+//                }
+//            }
+//
+//            public void caught(Throwable throwable) {
+//                //log
+//            }
+//        });
 
-        FutureAdapter future = (FutureAdapter) RpcContext.getContext().getFuture();
-        future.getFuture().setCallback(new ResponseCallback() {
-            public void done(Object o) {
-                RpcResult rpcResult = ((RpcResult) o);
-                if (rpcResult.hasException()) {
-                    //log
-                } else {
-                    System.out.println(rpcResult.getValue());
-                }
-            }
-
-            public void caught(Throwable throwable) {
-                //log
+        Future<String> future = RpcContext.getContext().asyncCall(new Callable<String>() {
+            public String call() throws Exception {
+                return helloService.say();
             }
         });
+
+        System.out.println(future.get());
+
     }
 }
