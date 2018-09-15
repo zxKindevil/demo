252ee0a925238de5f7b0e033ffc0dfb93236bf9a
zhangxin <zhangxin@benmu-health.com>
2018-09-16 01:44:14 +0800

    up

--- jdk8/src/main/java/com/kindevil/demo/okhttp/OkhttpTest.java
+++ jdk8/src/main/java/com/kindevil/demo/okhttp/OkhttpTest.java
@@ -2,20 +2,46 @@ package com.kindevil.demo.okhttp;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Maps;
 import com.google.common.io.Files;
 import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.apache.commons.codec.Charsets;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public class OkhttpTest {
 
     public static void main(String[] args) throws IOException {
+        HashMap<String, String> map = Maps.newHashMap();
+        map.put("companyId", "200554");
+        map.put("positionFirstType", "全部");
+        map.put("schoolJob", "false");
+        map.put("pageNo", "1");
+        map.put("pageSize", "20");
+
+        Document ret = Jsoup.connect("https://www.lagou.com/gongsi/searchPosition.json")
+                .method(Connection.Method.POST)
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36")
+                .header("Content-Type","application/x-www-form-urlencoded; charset=UTF-8")
+                .data(map)
+                .cookie("cookie", "_ga=GA1.2.497251197.1517658632; user_trace_token=20180203195032-70220014-08d8-11e8-a69b-525400f775ce; LGUID=20180203195032-70220793-08d8-11e8-a69b-525400f775ce; JSESSIONID=ABAAABAAAGFABEF65B20F1EEA60EA2D2C6D314FFFDC76F3; _gid=GA1.2.1584489656.1535796586; index_location_city=%E5%8C%97%E4%BA%AC; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1535796586,1535797807,1535826203; LGSID=20180902192455-d1a407a3-aea2-11e8-b416-5254005c3644; SEARCH_ID=5ecdd6575dd144fba33084de8d072fa0; TG-TRACK-CODE=hpage_code; _gat=1; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1535889287; LGRID=20180902195446-fcfb8886-aea6-11e8-81e9-525400f775ce")
+                .ignoreContentType(true)
+                .get();
+
+        System.out.println(ret.body().toString());
+
+    }
+
+    public static void testb(String[] args) throws IOException {
         File file = new File("/Users/zhangxin/temp.txt");
         List<String> strings = Files.readLines(file, Charsets.UTF_8);
         JsonNode jsonNode = new ObjectMapper().readTree(strings.get(0));
