aa93b4ff644a9c4e52f791cf814049c05c7bc786
zhangxin <zhangxin@benmu-health.com>
2018-09-17 03:27:21 +0800

    up

--- src/main/scala/com/leetcode/leetcode_746_minCostClimbingStairs.scala
+++ src/main/scala/com/leetcode/leetcode_746_minCostClimbingStairs.scala
@@ -3,6 +3,11 @@ package com.leetcode
 /** https://leetcode-cn.com/problems/min-cost-climbing-stairs/description/
   * 动态规划
   *
+  * 用dp[i] 表示爬 i 个台阶所需要的成本，所以dp[0]=0，dp[1]=0
+  * *
+  * 每次爬 i 个楼梯，计算的都是从倒数第一个结束，还是从倒数第二个结束，由此可以总结动态转移方程为
+  * *
+  * dp[i] = min{dp[i-2]+cost[i-2] , dp[i-1]+cost[i-1]};
   */
 class leetcode_746_minCostClimbingStairs {
 
